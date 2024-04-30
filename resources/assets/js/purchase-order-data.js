429d72b58ca5bf71a939366bc2e536589aa8252d
DerickCc <derickchainatra@gmail.com>
2024-05-01 04:48:57 +0700

    fix wrong path

--- resources/assets/js/purchase-order-data.js
+++ resources/assets/js/purchase-order-data.js
@@ -91,8 +91,8 @@ var poTable = $('#poDatatable').DataTable({
 });
 
 var exportAndAddButton = $(`
-  <a class="btn btn-primary float-end ms-3" href="sales-order/create">Tambah</a>
-  <a class="${userData.role == 'Admin' ? 'btn btn-success' : ''} float-end export" href="sales-order/export" ${userData.role == 'Admin' ? '' : 'hidden'}>Export</a>
+  <a class="btn btn-primary float-end ms-3" href="purchase-order/create">Tambah</a>
+  <a class="${userData.role == 'Admin' ? 'btn btn-success' : ''} float-end export" href="purchase-order/export" ${userData.role == 'Admin' ? '' : 'hidden'}>Export</a>
 `);
 
 $('.dataTables_length').append(exportAndAddButton);
